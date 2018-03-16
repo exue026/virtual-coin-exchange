bad1269c40b0929d410fdc4c33c183b729eae7ab
Ethan Xue <ethan.xue@gmail.com>
2018-03-16 00:22:47 -0400

    ok ok refresh everything

--- client/src/App.js
+++ client/src/App.js
@@ -3,36 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      response: '',
-    }
-  }
-
-  componentDidMount() {
-    this.hitApi()
-      .then(res => this.setState({ response: res.data }))
-      .catch(error => console.log(error))
-  }
-
-  hitApi = async() => {
-    const response = await fetch('/api')
-    const body = await response.json()
-
-    if (response.status !== 200) throw Error(body.message)
-    return body
-  }
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">{this.state.response}</h1>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/api', (req, res, next) => {
-  res.send({ data: 'Hello, World!' });
-});
+  const hello = 'hi'
+  res.send({ data: hello })
+})
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`Server listening on port ${port}`));
