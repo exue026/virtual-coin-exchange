94518a1a4dda2cf6d9b3d58304d3ee50abf23935
Ethan Xue <ethan.xue@gmail.com>
2018-04-01 18:16:08 -0400

    fixed up babel to support async/await

--- server/api/auth/register.js
+++ server/api/auth/register.js
@@ -5,42 +5,37 @@ import User from '../../models/user'
 
 const router = express.Router()
 
-const isRegistered = (email) => {
-  return User.findOne({
+const isRegistered = async(email) => {
+  const results = await User.findOne({
     email: email,
   }).count()
-    .then(results => {
-      return results > 0
-    })
+  return results > 0
 }
 
-router.post('/', (req, res, next) => {
+router.post('/', async(req, res, next) => {
   if (!req.body.username || !req.body.email || !req.body.password) {
     return res.status(400).send({
       error: 'Missing credentials.'
     })
   }
-  isRegistered(req.body.email)
-    .then(isRegistered => {
-      if (isRegistered) {
-        return res.status(400).send({
-          error: 'This user already exists.'
-        })
-      }
-      const user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-        games: [],
-      })
-      user.save()
-        .then(results => {
-          return res.status(200).send({ data: results })
-        })
-        .catch(error => {
-          return res.status(500).send({ error, })
-        })
+  if (await isRegistered(req.body.email)) {
+    return res.status(400).send({
+      error: 'This user already exists.'
+    })
+  }
+  const user = new User({
+    _id: new mongoose.Types.ObjectId(),
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    games: [],
+  })
+  user.save()
+    .then(results => {
+      return res.status(200).send({ data: results })
+    })
+    .catch(error => {
+      return res.status(500).send({ error, })
     })
 })
 
