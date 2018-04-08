7586ddce320124eb007b37a131cfa7a53a481bdb
Ethan Xue <ethan.xue@gmail.com>
2018-04-08 14:05:24 -0400

    stuff

--- server/api/index.js
+++ server/api/index.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import cheerio from 'cheerio'
+import rp from 'request-promise'
 
 import users from './users'
 import auth from './auth'
@@ -7,5 +9,22 @@ const router = express.Router()
 
 router.use('/auth', auth)
 router.use('/users', users)
+
+router.get('/test', async(req, res, next) => {
+
+  //--------------------------
+  const options = {
+    uri: `https://coinmarketcap.com/historical/`,
+    transform: (body) => {
+      return cheerio.load(body);
+    }
+  };
+
+  const $  = await rp(options)
+  console.log($('.list-unstyled').length);
+  //--------------------------
+
+  res.send('hi\n')
+})
 
 export default router
