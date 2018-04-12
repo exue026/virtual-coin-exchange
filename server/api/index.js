2f856eeee26cac230d8386d895f557969c41cbf4
Ethan Xue <ethan.xue@gmail.com>
2018-04-12 00:36:55 -0400

    added endpoint for getting coin data

--- server/api/index.js
+++ server/api/index.js
@@ -2,10 +2,12 @@ import express from 'express'
 
 import users from './users'
 import auth from './auth'
+import coins from './coins'
 
 const router = express.Router()
 
 router.use('/auth', auth)
 router.use('/users', users)
+router.use('/coins', coins)
 
 export default router
