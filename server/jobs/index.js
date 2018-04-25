af0c4f4d4f7d0264876c1afc2827cec23d209310
Ethan Xue <ethan.xue@gmail.com>
2018-04-24 23:20:55 -0700

    modified jobs

--- server/jobs/index.js
+++ server/jobs/index.js
@@ -3,16 +3,14 @@ import Agenda from 'agenda'
 import CONFIG from '../config'
 import { addWeeklyCoinData } from './coins'
 
-export const setupJobs = () => {
-  const agenda = new Agenda({ db: { address: CONFIG.MONGO_URL } })
+const agenda = new Agenda({ db: { address: CONFIG.MONGO_URL } })
 
-  agenda.define('update coin history', async(job, done) => {
-    await addWeeklyCoinData()
-    done()
-  })
+agenda.define('update coin history', async(job, done) => {
+  await addWeeklyCoinData()
+  done()
+})
 
-  agenda.on('ready', () => {
-    agenda.every('1 week', 'update coin history')
-    agenda.start()
-  })
-}
+agenda.on('ready', () => {
+  agenda.every('1 week', 'update coin history')
+  agenda.start()
+})
